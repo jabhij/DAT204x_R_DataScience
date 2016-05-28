5b7199f195a0d5c66e4de0692b99cf3fca2ad6c2
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-29 00:50:29 +0530

    Operations.r

---
+++ BASICS/LAB1/Operations.r
@@ -0,0 +1,24 @@
+Instructions --
+Type 2^5 in the editor to calculate 2 to the power 5.
+Type 28 %% 6 to calculate 28 modulo 6.
+Click 'Submit Answer' and have a look at the R output in the console.
+Note how the # symbol is used to add comments on the R code.
+
+
+# Addition
+5 + 5 
+
+# Subtraction
+5 - 5 
+
+# Multiplication
+3 * 5
+
+ # Division
+(5 + 5) / 2 
+
+# Exponentiation
+2 ^ 5
+
+# Modulo
+28 %% 6
