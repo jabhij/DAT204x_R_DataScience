0d0d9a114b24f218a233082b848c4cf153be73ae
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-29 00:53:08 +0530

    Var_Assignment.r

---
+++ BASICS/LAB1/Var_Assignment.r
@@ -0,0 +1,8 @@
+Complete the code in the editor such that it assigns the value 42 to the variable x in the editor. Click 'Submit Answer'. Notice that when
+you ask R to print x, the value 42 appears.
+
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+x
