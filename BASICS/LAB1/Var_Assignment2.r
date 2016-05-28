ddfeceba8cf42423ebaaddd318bf035e0da92647
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-29 00:54:31 +0530

    Var_Assignment2.r

---
+++ BASICS/LAB1/Var_Assignment2.r
@@ -0,0 +1,12 @@
+Instructions --
+
+Using <-, assign the value 5 to my_apples below the first comment.
+Type my_apples below the second comment. This will print out the value of my_apples.
+Click 'Submit Answer', and look at the console: you see that the number 5 is printed. So R now links the variable my_apples to the value 5
+
+
+# Assign the value 5 to the variable called my_apples
+my_apples <- 5
+
+# Print out the value of the variable my_apples
+my_apples
