2d0b3bf19bac2ba18d2ce6fa176ecb46dd687231
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-29 00:55:49 +0530

    Var_Assignment3.r

---
+++ BASICS/LAB1/Var_Assignment3.r
@@ -0,0 +1,16 @@
+Instructions --
+
+Assign to my_oranges the value 6.
+Add the variables my_apples and my_oranges and have R simply print the result.
+Combine the variables my_apples and my_oranges into a new variable my_fruit, which is the total amount of fruits in your fruit basket.
+
+
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+my_oranges <- 6
+
+# Add these two variables together and print the result
+my_fruit <- my_apples + my_oranges
+
+# Create the variable my_fruit
+my_fruit
