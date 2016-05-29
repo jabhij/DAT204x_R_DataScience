b198cc084f270856ca24bc10a4c561bccfc5e1aa
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-29 19:28:45 +0530

    Basic_Data_Types

---
+++ BASICS/LAB2/L1.py
@@ -0,0 +1,19 @@
+Instructions  --
+
+Change the value of the:
+my_numeric variable to 42.
+my_character variable to "forty-two". Note that the quotation marks indicate that "forty-two" is a character.
+my_logical variable to FALSE.
+Note that R is case sensitive!
+
+# What is the answer to the universe?
+my_numeric <- 42.5
+my_numeric <- 42
+
+# The quotation marks indicate that the variable is of type character
+my_character <- "some text"
+my_character <- "forty-two"
+
+# Change the value of my_logical
+my_logical <- TRUE
+my_logical <- FALSE
