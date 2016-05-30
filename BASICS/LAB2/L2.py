396ed0ad444e0c0fabdab4621b46d2eb902585ba
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-30 22:36:04 +0530

    Base_Operations

---
+++ BASICS/LAB2/L2.py
@@ -0,0 +1,18 @@
+Instructions --
+
+Click 'Submit Answer' and read the error message. Make sure to understand why this did not work.
+Adjust the code so that R knows you have 6 oranges and thus a fruit basket with 11 pieces of fruit.
+
+# Assign a value to the variable called my_apples
+my_apples <- 5 
+
+# Print out the value of my_apples
+my_apples       
+
+# Assign a value to the variable my_oranges and print it out
+my_oranges <- "six" 
+my_oranges <- 6
+
+# New variable that contains the total amount of fruit
+my_fruit <- my_apples + my_oranges 
+my_fruit
