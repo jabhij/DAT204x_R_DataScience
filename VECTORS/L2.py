536b4ba17b04b9a07df59a8487bbdfa48c282779
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-31 21:19:06 +0530

    Creating a vector

---
+++ VECTORS/L2.py
@@ -0,0 +1,9 @@
+Instructions --
+
+Assign the winnings/losses for roulette to the variable roulette_vector.
+
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday: roulette_vector
+roulette_vector <- c(-24, -50, 100, -350, 10)
