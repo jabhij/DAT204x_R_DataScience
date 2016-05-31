fade0b736c2c2bcb452535e8228ec9e28978e944
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-01 01:06:05 +0530

    Summing & subtracting vectors

---
+++ VECTORS/LAB2/L1.r
@@ -0,0 +1,23 @@
+Instructions --
+
+Take the element-wise sum of the variables A_vector and B_vector and it assign to total_vector. The result should be a vector.
+Inspect the result by printing total_vector to the console.
+Do the same thing, but this time subtract B_vector from A_vector and assign the result to diff_vector.
+Finally, print diff_vector to the console as well.
+--------------------
+
+# A_vector and B_vector have already been defined for you
+A_vector <- c(1, 2, 3)
+B_vector <- c(4, 5, 6)
+
+# Take the sum of A_vector and B_vector: total_vector
+  total_vector <- A_vector + B_vector
+  
+# Print total_vector
+total_vector
+
+# Calculate the difference between A_vector and B_vector: diff_vector
+diff_vector <- A_vector - B_vector
+
+# Print diff_vector
+diff_vector
