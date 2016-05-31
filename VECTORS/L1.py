56ef94b31d83697143746cc282fde61b4d906224
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-31 21:45:18 +0530

    Delete L1.py

--- VECTORS/L1.py
+++
@@ -1,9 +0,0 @@
-Instructions --
-
-Build a vector, boolean_vector, that contains the three elements: TRUE, FALSE and TRUE (in that order).
-
-numeric_vector <- c(1, 10, 49)
-character_vector <- c("a", "b", "c")
-
-# Create boolean_vector
-boolean_vector <- c(TRUE, FALSE, TRUE)
