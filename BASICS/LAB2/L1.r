1ba4a72f087b00b28119524e654c26951902700f
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-31 21:55:12 +0530

    Basic Data Types

--- BASICS/LAB2/L1.r
+++ BASICS/LAB2/L1.r
@@ -5,6 +5,7 @@ my_numeric variable to 42.
 my_character variable to "forty-two". Note that the quotation marks indicate that "forty-two" is a character.
 my_logical variable to FALSE.
 Note that R is case sensitive!
+----------------
 
 # What is the answer to the universe?
 my_numeric <- 42.5
