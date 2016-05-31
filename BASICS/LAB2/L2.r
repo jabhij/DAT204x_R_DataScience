66ffeb6d2f508b0fe463a2d61c571b5c53f53f4a
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-31 21:55:34 +0530

    Base Operations

--- BASICS/LAB2/L2.r
+++ BASICS/LAB2/L2.r
@@ -2,6 +2,7 @@ Instructions --
 
 Click 'Submit Answer' and read the error message. Make sure to understand why this did not work.
 Adjust the code so that R knows you have 6 oranges and thus a fruit basket with 11 pieces of fruit.
+-----------------
 
 # Assign a value to the variable called my_apples
 my_apples <- 5 
