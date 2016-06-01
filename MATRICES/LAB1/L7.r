0b5fb0bd5d993f39f8f80bbb8a04fd1f4e4780bb
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-02 00:14:35 +0530

    Using colSums()

---
+++ MATRICES/LAB1/L7.r
@@ -0,0 +1,11 @@
+Instructions --
+
+Calculate the total revenue for the US and the non-US region and assign it to total_revenue_vector.
+-----------------
+
+# Print box office Star Wars
+all_wars_matrix
+
+# Total revenue for US and non-US: total_revenue_vector
+total_revenue_vector <- colSums (all_wars_matrix)
+total_revenue_vector
