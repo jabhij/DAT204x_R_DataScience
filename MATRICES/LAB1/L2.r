558f497dfa9dd94bf9a5da191420d6baa786fa9f
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-02 00:03:31 +0530

    Analyzing matrices2

---
+++ MATRICES/LAB1/L2.r
@@ -0,0 +1,13 @@
+Instructions --
+
+Again, construct the matrix star_wars_matrix with one row for each movie.
+----------------
+
+# Star Wars box office in millions (!)
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+
+# Create star_wars_matrix
+star_wars_matrix <- rbind(new_hope, empire_strikes, return_jedi)
+star_wars_matrix 
