0b82d320542a4fc97a008f117d1485c1340b639b
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-03 00:48:12 +0530

    Alternative ways of subsetting

---
+++ MATRICES/LAB2/L4.r
@@ -0,0 +1,25 @@
+Instructions --
+
+Using names, select the US revenues for "A New Hope" and "The Empire Strikes Back".
+Using logical vectors, select the last two rows and both columns from star_wars_matrix.
+Finally, select the non-US revenue for "The Empire Strikes Back" with whatever technique you like.
+----------------
+
+# star_wars_matrix is already defined in your workspace
+star_wars_matrix
+
+# Select the US revenues for "A New Hope" and "The Empire Strikes Back"
+star_wars_matrix[c('A New Hope', 'The Empire Strikes Back'), 'US']
+
+# Select the last two rows and both columns
+star_wars_matrix[c(FALSE, TRUE, TRUE), c(TRUE, TRUE)]
+
+# Select the non-US revenue for "The Empire Strikes Back"
+# Using Index --
+star_wars_matrix[2 , 2]
+
+# Using Names --
+star_wars_matrix[c('The Empire Strikes Back'), 'non-US']
+
+#Using Logical Vectors --
+star_wars_matrix[c(FALSE, TRUE), c(FALSE, TRUE)]
