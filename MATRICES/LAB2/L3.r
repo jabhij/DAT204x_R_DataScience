fddb8135779dcbc58861d6f55c1dd36a4dbde13c
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-03 00:34:18 +0530

    Creating Submatrices

---
+++ MATRICES/LAB2/L3.r
@@ -0,0 +1,10 @@
+Instructions --
+
+Select all revenue figures for "A New Hope" and "Return of the Jedi" from star_wars_matrix.
+-----------------
+
+# star_wars_matrix is already defined in your workspace
+star_wars_matrix
+
+# All figures for "A New Hope" and "Return of the Jedi"
+star_wars_matrix[c(1,3), c(1,2)]
