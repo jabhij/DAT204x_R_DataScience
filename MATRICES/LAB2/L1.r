bb89287725f43c87d77a10b36b35d3f51fc7f173
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-02 23:48:47 +0530

    Select Elements

---
+++ MATRICES/LAB2/L1.r
@@ -0,0 +1,14 @@
+Instructions --
+
+Select the US box office figure for "The Empire Strikes Back".
+Select the non-US box office number for "A New Hope" No need to assign these elements to new variables; simply print them.
+----------------
+
+# star_wars_matrix is already defined in your workspace
+star_wars_matrix
+
+# US box office revenue for "The Empire Strikes Back"
+star_wars_matrix[2,1]
+
+# non-US box office revenue for "A New Hope"
+star_wars_matrix[1,2]
