566a286b4da089f078a971de768bf7802a786b72
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-03 00:52:22 +0530

    Subsetting: The final challenge

---
+++ MATRICES/LAB2/L6.r
@@ -0,0 +1,9 @@
+Instructions --
+
+Combine the matrices view_count_1 and view_count_2 in a new matrix, view_count_all, using cbind().
+From view_count_all, subset the view counts for the three loudest debaters (Rachel, Walter and Dave). You can use subsetting by indices, 
+names or a logical vector here. Store the result in the matrix view_count_loud.
+Use the function colSums() to calculate the total number of times the loud debaters have seen any of the Star Wars movies. Store the 
+result in a vector in total_views_loud.
+---------------
+
