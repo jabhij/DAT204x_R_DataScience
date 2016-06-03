b7cbe720e23f50ff77985900a4e16ca13d53bbea
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-03 11:17:33 +0530

    Create L1.r

---
+++ MATRICES/LAB3/L1.r
@@ -0,0 +1,5 @@
+Instructions --
+
+Assign the matrix with the estimated number of Non-US and US visitors (in millions) for the three movies to visitors.
+Print the resulting variable to the console.
+-------------
