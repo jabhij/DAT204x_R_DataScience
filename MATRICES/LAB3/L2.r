8ca4a028525edcf0bf722361f9af90b206e8a93e
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-04 02:22:08 +0530

    Arithmetic with matrices(2)

---
+++ MATRICES/LAB3/L2.r
@@ -0,0 +1,7 @@
+Instructions --
+
+Assign to visitors the matrix with your estimated number of Non-US and US visitors for the three movies.
+Assign to average_us_visitors the average number of visitors in the US for a Star Wars movie. You'll need matrix subsetting here!
+Assign to average_non_us_visitors the average number of visitors in non-US areas.
+-----------------
+
