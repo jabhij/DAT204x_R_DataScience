48a1667d095b540de425c1b46de78912b7d71c4e
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-04 18:10:42 +0530

    Ordered factors

---
+++ FACTOR/LAB/L6.r
@@ -0,0 +1,14 @@
+Instructions --
+
+As a first step, assign speed_vector knowing that:
+
+Analyst 1 is OK,
+Analyst 2 is Slow,
+Analyst 3 is Slow,
+Analyst 4 is OK and
+Analyst 5 is Fast.
+No need to specify these are factors yet.
+---------------
+
+# Create speed_vector
+speed_vector <- c('OK', 'Slow', 'Slow', 'OK', 'Fast')
