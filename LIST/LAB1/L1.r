f26bd6fbfdf84e93e3112ebb31abd32062889ddc
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-05 02:16:41 +0530

    Create a list

--- LIST/LAB1/L1.r
+++ LIST/LAB1/L1.r
@@ -1,1 +1,17 @@
+Instructions --
 
+Construct a list, named my_list, that contains the variables my_vector, my_matrix and my_factor as list components.
+----------------
+
+# Numeric vector: 1 up to 10
+my_vector <- 1:10 
+
+# Numeric matrix: 1 up to 9
+my_matrix <- matrix(1:9, ncol = 3)
+
+# Factor of sizes
+my_factor <- factor(c("M","S","L","L","M"), ordered = TRUE, 
+                    levels = c("S","M","L"))
+
+# Construct my_list with these different elements
+my_list <- list(my_vector, my_matrix, my_factor)
