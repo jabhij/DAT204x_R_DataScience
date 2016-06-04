cbad64753801ea1ab6f6260644af118051a833c0
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-04 17:37:14 +0530

    Factor levels

---
+++ FACTOR/LAB/L2.r
@@ -0,0 +1,16 @@
+Instructions --
+
+Convert the character vector survey_vector into a factor vector, survey_factor, with the levels "Right" and "Left".
+Print survey_factor to inspect its contents.
+-----------------
+
+# Definition of survey_vector
+survey_vector <- c("R", "L", "L", "R", "R")
+
+# Encode survey_vector as a factor with the correct names: survey_factor
+survey_factor <- factor(survey_vector, ordered=TRUE,
+                 levels <- c('R', 'L'),
+                 labels <- c('Right', 'Left'))
+
+# Print survey_factor
+survey_factor
