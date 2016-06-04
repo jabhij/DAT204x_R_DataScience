ff1a4ec0c1d5ea996f7d22eae0179b014410dde8
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-04 17:31:09 +0530

    Vector to factor

--- FACTOR/LAB/L1.r
+++ FACTOR/LAB/L1.r
@@ -1,2 +1,16 @@
 Instructions --
 
+Assign to hand_factor the character vector hand_vector converted to a factor. Look at the console and note that R prints out the factor
+levels below the actual values.
+Have a look at the underlying structure of hand_factor using str().
+-----------------
+
+# Definition of hand_vector
+hand_vector <- c("Right", "Left", "Left", "Right", "Left")
+
+# Convert hand_vector to a factor: hand_factor
+hand_factor <- factor(hand_vector)
+hand_factor 
+
+# Display the structure of hand_factor
+str(hand_factor)
