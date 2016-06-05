480e2796e249e9fd748caa326aebfd5c24f35f5e
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-05 12:25:54 +0530

    VectorSubsetting VS ListSubsetting

---
+++ LIST/LAB2/L3.r
@@ -0,0 +1,13 @@
+shining_list has been extended with even more information (source: imagination); the list is available in the workspace. 
+Which of the following statements are correct?
+
+A) shining_list$box[1,2] gives the non-US box office of the first release.
+B) shining_list[[c(2,4)]] creates a list containing the actors vector and the box office matrix.
+C) shining_list[[c(2,4)]] returns "Scatman Crothers".
+D) shining_list$reviews[1] > shining_list[[3]][3] is invalid syntax.
+
+A and B  
+A and C (CORRECT)
+A and D  
+C and D  
+B and D 
