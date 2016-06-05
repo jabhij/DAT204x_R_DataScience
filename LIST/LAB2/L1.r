e1f9d94142695b25b79c0645e2d60d358a7cdb52
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-05 12:12:26 +0530

    Selecting elements from a list

--- LIST/LAB2/L1.r
+++ LIST/LAB2/L1.r
@@ -1,1 +1,21 @@
+Instructions --
 
+Select the actors from shining_list and assign the result to act.
+Create a new list containing only the title and the reviews of shining_list; save the new list in sublist.
+Display the structure of sublist.
+
+----------------
+
+# shining_list is already defined in the workspace
+shining_list
+
+# Actors from shining_list: act
+act <- shining_list[['actors']]
+act
+
+# List containing title and reviews from shining_list: sublist
+sublist <- shining_list[c('title', 'reviews')]
+sublist
+
+# Display structure of sublist
+str(sublist)
