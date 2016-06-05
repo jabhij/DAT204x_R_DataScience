6988b8bd07c14d7d130d19aaba4cb5a1357be980
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-06 00:31:13 +0530

    Have a look at your data set

---
+++ DATA_FRAMES/LAB1/L1.r
@@ -0,0 +1,15 @@
+Instructions --
+
+Print the first observations of the mtcars data set.
+Use the tail() function to display the last observations.
+Finally, display the overall dimensions of the mtcars data frame with dim().
+----------------
+
+# Print the first observations of mtcars
+head(mtcars)
+
+# Print the last observations of mtcars
+tail(mtcars)
+
+# Print the dimensions of mtcars
+dim(mtcars)
