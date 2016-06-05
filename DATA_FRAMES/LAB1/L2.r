c36e2a1760b3204ab1dbc79d682079cc1ca2329c
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-06 00:34:16 +0530

    Have a look at the structure

---
+++ DATA_FRAMES/LAB1/L2.r
@@ -0,0 +1,7 @@
+Instructions --
+
+Investigate the structure of mtcars. Make sure that you see the same numbers, variables and data types as mentioned above.
+---------------
+
+# Investigate the structure of the mtcars data set
+str(mtcars)
