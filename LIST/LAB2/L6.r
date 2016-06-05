1489b0a0f9cae153c878917448a6fa7b8de8b746
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-05 13:58:04 +0530

    Create L6.r

---
+++ LIST/LAB2/L6.r
@@ -0,0 +1,9 @@
+Instructions-- 
+
+Create a new list named key_skills, that contains three elements (in this order):
+
+The second element of the topics vector inside the skills list.
+The second element of the context factor inside the skills list.
+The last element of the logical vector inside the list_info list, that's inside skills.
+--------------
+
