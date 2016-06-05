69cc0398e231866db553a7f325cdaf621cf77e7f
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-06 00:46:31 +0530

    Creating a data frame

---
+++ DATA_FRAMES/LAB1/L3.r
@@ -0,0 +1,19 @@
+Instructions --
+
+Use the function data.frame() to construct planets_df.
+Make sure that you've actually created a data frame with 8 observations and 5 variables with str().
+--------------
+
+# Definition of vectors
+planets <- c("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+type <- c("Terrestrial planet", "Terrestrial planet", "Terrestrial planet", 
+          "Terrestrial planet", "Gas giant", "Gas giant", "Gas giant", "Gas giant")
+diameter <- c(0.382, 0.949, 1, 0.532, 11.209, 9.449, 4.007, 3.883)
+rotation <- c(58.64, -243.02, 1, 1.03, 0.41, 0.43, -0.72, 0.67)
+rings <- c(FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE)
+
+# Create a data frame: planets_df
+planets_df <- data.frame(planets, type, diameter, rotation, rings)
+
+# Display the structure of planets_df
+str(planets_df)
