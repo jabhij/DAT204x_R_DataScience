6ff725b62b6c6f2ff112e351d7913dfdc62039dc
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-06 16:22:14 +0530

    Sorting

---
+++ DATA_FRAMES/LAB2/L8.r
@@ -0,0 +1,8 @@
+Instructions --
+
+Experiment with the order() function in the console. Click 'Submit Answer' when you are ready to continue.
+---------------
+
+# Just play around with the order function in the console to see how it works!
+num <- c(2, 1, 5, 4, 3)
+order(num)
