bac67b7333c9013f453f9afb74678ae0ead8b041
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-06 15:35:14 +0530

    Selection of data frame elements

---
+++ DATA_FRAMES/LAB2/L1.r
@@ -0,0 +1,25 @@
+Instructions --
+
+Select the type of Mars; store the factor in mars_type.
+Store the entire rotation column in rotation as a vector.
+Create a data frame, closest_planets_df, that contains all data on the first three planets.
+Likewise, build the data frame furthest_planets_df that contains all data on the last three planets.
+-------------
+
+# planets_df is pre-loaded
+planets_df
+
+# The type of Mars: mars_type
+mars_type <- planets_df[4, 2]
+mars_type
+
+# Entire rotation column: rotation
+rotation <- planets_df['rotation']
+
+# First three planets: closest_planets_df
+closest_planets_df <- data.frame(planets_df[1:3, 1:5])
+closest_planets_df
+
+# Last three planets: furthest_planets_df
+furthest_planets_df <- data.frame(planets_df[5:7, 1:5])
+furthest_planets_df
