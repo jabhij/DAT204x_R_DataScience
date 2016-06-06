efb5003eb8714b369e2ea2eeb74103b5e955115b
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-06 15:45:10 +0530

    Only planets with rings

---
+++ DATA_FRAMES/LAB2/L3.r
@@ -0,0 +1,14 @@
+Instructions --
+
+Make use of the $ sign to create the variable rings_vector that contains the entire has_rings column in the planets_df data frame.
+Print the rings_vector; it should be a vector.
+-----------------
+
+# planets_df is pre-loaded in your workspace
+planets_df
+
+# Create rings_vector
+rings_vector <- planets_df$has_rings
+
+# Print rings_vector
+rings_vector
