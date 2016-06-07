82eb72bf41bc96a1e0dec0c563b96b9342d9a3a7
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-08 02:34:44 +0530

    Multiple plots with par()

---
+++ GRAPHICS/LAB3/L1.r
@@ -0,0 +1,10 @@
+Instructions --
+
+Instructions
+List all the graphical parameters that are currently active in your session, by running par().
+Next, use par() to set the mfrow parameter: R should plot figures on a 2-by-1 grid (2 rows, 1 column).
+Build two plots:
+A scatterplot that plots the votes (x-axis) against the rating (y-axis) variable of movies.
+A histogram of the votes variable
+-----------------
+
