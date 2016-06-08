6d779c459b82b0ee8f420c49952259e618e640ad
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-08 21:55:08 +0530

    Create L2.r

---
+++ GRAPHICS/LAB3/L2.r
@@ -0,0 +1,9 @@
+Instructions --
+
+Build a 2-by-2 matrix, grid, that will be used for positioning the 3 subplots as specified above.
+Use layout() in combination with grid.
+Build three plots for the movies data frame (in this order):
+A scatter plot of rating (x-axis) versus runtime (y-axis).
+A scatter plot of votes (x-axis) versus runtime (y-axis).
+A boxplot of the runtime (use boxplot())
+----------------
