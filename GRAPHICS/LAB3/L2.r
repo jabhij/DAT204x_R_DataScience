47c8e231a63b2be398a830c4041d24a4b5c50130
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-08 22:55:51 +0530

    Complex layouts!

--- GRAPHICS/LAB3/L2.r
+++ GRAPHICS/LAB3/L2.r
@@ -7,3 +7,17 @@ A scatter plot of rating (x-axis) versus runtime (y-axis).
 A scatter plot of votes (x-axis) versus runtime (y-axis).
 A boxplot of the runtime (use boxplot())
 ----------------
+
+# movies is pre-loaded in your workspace
+movies
+
+# Build the grid matrix
+grid <- matrix(c(1, 2, 3, 3), nrow = 2)
+
+# Specify the layout
+layout(grid)
+
+# Build three plots
+plot(movies$rating , movies$runtime)
+plot(movies$votes , movies$runtime)
+boxplot(movies$runtime)
